8b5385018f4019ff4622944ddb29bc346e461a21
Chris Piccoli <cap9763@rit.edu>
2021-09-07 15:22:42 -0400

    Implemented CandidateMoves for Knight

--- src/main/java/model/Knight.java
+++ src/main/java/model/Knight.java
@@ -1,15 +1,26 @@
 package model;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class Knight extends Piece {
+    public static List candidateMoves = Arrays.asList(new Move[]{
+                                                        new Move(new int[]{-1,2,0}),
+                                                        new Move(new int[]{1,2,0}),
+                                                        new Move(new int[]{2,-1,0}),
+                                                        new Move(new int[]{2,1,0}),
+                                                        new Move(new int[]{-1,-2,0}),
+                                                        new Move(new int[]{1,-2,0}),
+                                                        new Move(new int[]{-2,-1,0}),
+                                                        new Move(new int[]{-2,1,0})
+                                                    });
     public Knight(int id, Color col) {
         super(id, col);
     }
 
     @Override
     public List getCandidateMoves() {
-        return null;
+        return candidateMoves;
     }
     @Override
     public String toString(){
