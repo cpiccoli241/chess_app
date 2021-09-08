92a07dce2e8840d1672724a2496febf3feee9bc1
Chris Piccoli <cap9763@rit.edu>
2021-09-08 16:37:23 -0400

    Created ChessBoardRepTest Fixed numberings on on test cases

--- src/test/java/model/ChessBoardRepTest.java
+++ src/test/java/model/ChessBoardRepTest.java
@@ -16,19 +16,19 @@ public class ChessBoardRepTest {
         // test white moving pawn forward 1
         bd.makeMove(app.convertFromPGNToMove("pd2d3"));
         assertTrue(bd.getSquare(app.convertFromPGNToSquare("d2"))==0);
-        assertTrue(bd.getSquare(app.convertFromPGNToSquare("d2"))==14);
+        assertTrue(bd.getSquare(app.convertFromPGNToSquare("d3"))==15);
         //test black moving pawn forward 1
         bd.makeMove(app.convertFromPGNToMove("pd7d6"));
         assertTrue(bd.getSquare(app.convertFromPGNToSquare("d7"))==0);
-        assertTrue(bd.getSquare(app.convertFromPGNToSquare("d6"))==-14);
+        assertTrue(bd.getSquare(app.convertFromPGNToSquare("d6"))==-15);
         //white moving pawn forward 2
         bd.makeMove(app.convertFromPGNToMove("pe2e4"));
         assertTrue(bd.getSquare(app.convertFromPGNToSquare("e2"))==0);
-        assertTrue(bd.getSquare(app.convertFromPGNToSquare("e4"))==15);
+        assertTrue(bd.getSquare(app.convertFromPGNToSquare("e4"))==14);
         //black moving pawn forwad 2
         bd.makeMove(app.convertFromPGNToMove("pe7e5"));
         assertTrue(bd.getSquare(app.convertFromPGNToSquare("e7"))==0);
-        assertTrue(bd.getSquare(app.convertFromPGNToSquare("e5"))==15);
+        assertTrue(bd.getSquare(app.convertFromPGNToSquare("e5"))==-14);
 
         //Now lets try illegal moves
 
@@ -36,23 +36,23 @@ public class ChessBoardRepTest {
         // test white moving pawn backward 1
         bd.makeMove(app.convertFromPGNToMove("pd3d2"));
         assertTrue(bd.getSquare(app.convertFromPGNToSquare("d2"))==0);
-        assertTrue(bd.getSquare(app.convertFromPGNToSquare("d3"))==14);
+        assertTrue(bd.getSquare(app.convertFromPGNToSquare("d3"))==15);
 
         //test black moving pawn backward 1
         bd.setTurn(Color.BLACK);
         bd.makeMove(app.convertFromPGNToMove("pd6d7"));
         assertTrue(bd.getSquare(app.convertFromPGNToSquare("d7"))==0);
-        assertTrue(bd.getSquare(app.convertFromPGNToSquare("d6"))==-14);
+        assertTrue(bd.getSquare(app.convertFromPGNToSquare("d6"))==-15);
         //white moving pawn backward 2
         bd.setTurn(Color.WHITE);
         bd.makeMove(app.convertFromPGNToMove("pe4e2"));
         assertTrue(bd.getSquare(app.convertFromPGNToSquare("e2"))==0);
-        assertTrue(bd.getSquare(app.convertFromPGNToSquare("e4"))==15);
+        assertTrue(bd.getSquare(app.convertFromPGNToSquare("e4"))==14);
         //black moving pawn backward 2
         bd.setTurn(Color.BLACK);
         bd.makeMove(app.convertFromPGNToMove("pe5e7"));
         assertTrue(bd.getSquare(app.convertFromPGNToSquare("e7"))==0);
-        assertTrue(bd.getSquare(app.convertFromPGNToSquare("e5"))==15);
+        assertTrue(bd.getSquare(app.convertFromPGNToSquare("e5"))==-14);
     }
     @Test
     public void testPawnCapture(){
@@ -60,7 +60,7 @@ public class ChessBoardRepTest {
         CMDChessApp app = new CMDChessApp();
         //white moving pawn forward 2
         bd.makeMove(app.convertFromPGNToMove("pe2e4"));
-        //black moving pawn forwad 2
+        //black moving pawn forward 2
         bd.makeMove(app.convertFromPGNToMove("pe7e5"));
 
         //Forwards Capture Moves
@@ -100,18 +100,29 @@ public class ChessBoardRepTest {
         // test white moving knight
         bd.makeMove(app.convertFromPGNToMove("hg1f3"));
         assertTrue(bd.getSquare(app.convertFromPGNToSquare("g1"))==0);
-        assertTrue(bd.getSquare(app.convertFromPGNToSquare("f3"))==7);
+        assertTrue(bd.getSquare(app.convertFromPGNToSquare("f3"))==2);
         //test black moving knight
         bd.makeMove(app.convertFromPGNToMove("hg8f6"));
         assertTrue(bd.getSquare(app.convertFromPGNToSquare("g8"))==0);
-        assertTrue(bd.getSquare(app.convertFromPGNToSquare("f6"))==-7);
+        assertTrue(bd.getSquare(app.convertFromPGNToSquare("f6"))==-2);
         //knight again
-        bd.makeMove(app.convertFromPGNToMove("he2e4"));
+        bd.makeMove(app.convertFromPGNToMove("hf3e5"));
         assertTrue(bd.getSquare(app.convertFromPGNToSquare("f3"))==0);
-        assertTrue(bd.getSquare(app.convertFromPGNToSquare("e5"))==7);
+        assertTrue(bd.getSquare(app.convertFromPGNToSquare("e5"))==2);
         //black moving knight again
-        bd.makeMove(app.convertFromPGNToMove("he7e5"));
+        bd.makeMove(app.convertFromPGNToMove("hf6e4"));
         assertTrue(bd.getSquare(app.convertFromPGNToSquare("f6"))==0);
-        assertTrue(bd.getSquare(app.convertFromPGNToSquare("e4"))==-7);
+        assertTrue(bd.getSquare(app.convertFromPGNToSquare("e4"))==-2);
+
+        //test some illegal moves for knight
+        bd.makeMove(app.convertFromPGNToMove("he5e6"));
+        assertTrue(bd.getSquare(app.convertFromPGNToSquare("f3"))==0);
+        assertTrue(bd.getSquare(app.convertFromPGNToSquare("e5"))==2);
+        bd.makeMove(app.convertFromPGNToMove("he5e7"));
+        assertTrue(bd.getSquare(app.convertFromPGNToSquare("f3"))==0);
+        assertTrue(bd.getSquare(app.convertFromPGNToSquare("e5"))==2);
+        bd.makeMove(app.convertFromPGNToMove("he5d6"));
+        assertTrue(bd.getSquare(app.convertFromPGNToSquare("f3"))==0);
+        assertTrue(bd.getSquare(app.convertFromPGNToSquare("e5"))==2);
     }
 }
