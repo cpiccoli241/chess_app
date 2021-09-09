3ee3e40d423dd80e407bf4586e580d30e7234ba4
Chris Piccoli <cap9763@rit.edu>
2021-09-09 13:39:39 -0400

    fixed bug in bishop move by overriding piece isValidMove()

--- src/main/java/model/Bishop.java
+++ src/main/java/model/Bishop.java
@@ -3,29 +3,42 @@ package model;
 import java.util.Arrays;
 import java.util.List;
 
+import static java.lang.Math.abs;
+
 public class Bishop extends Piece {
     public Bishop(int id, Color col) {
         super(id, col);
     }
 
     @Override
+    /**
+     * Not usable rn @todo
+     */
     public List<Move> getCandidateMoves(int[] start) {
-        return Arrays.asList(
-                new Move(start,new int[]{2-start[0],2-start[1]}),
-                new Move(start,new int[]{3-start[0],3-start[1]}),
-                new Move(start,new int[]{4-start[0],4-start[1]}),
-                new Move(start,new int[]{5-start[0],5-start[1]}),
-                new Move(start,new int[]{6-start[0],6-start[1]}),
-                new Move(start,new int[]{7-start[0],7-start[1]}),
-                new Move(start,new int[]{8-start[0],8-start[1]}),
-                new Move(start,new int[]{8-start[0],2-start[1]}),
-                new Move(start,new int[]{7-start[0],3-start[1]}),
-                new Move(start,new int[]{6-start[0],4-start[1]}),
-                new Move(start,new int[]{5-start[0],5-start[1]}),
-                new Move(start,new int[]{4-start[0],6-start[1]}),
-                new Move(start,new int[]{3-start[0],7-start[1]}),
-                new Move(start,new int[]{2-start[0],8-start[1]})
+        List<Move> moves = Arrays.asList(
+                new Move(start,2-start[0],2-start[1]),
+                new Move(start,  3-start[0],3-start[1]),
+                new Move(start,  4-start[0],4-start[1]),
+                new Move(start,  5-start[0],5-start[1]),
+                new Move(start,  6-start[0],6-start[1]),
+                new Move(start,  7-start[0],7-start[1]),
+                new Move(start,  8-start[0],8-start[1]),
+                new Move(start,  8-start[0],2-start[1]),
+                new Move(start,  7-start[0],3-start[1]),
+                new Move(start,  6-start[0],4-start[1]),
+                new Move(start,  5-start[0],5-start[1]),
+                new Move(start,  4-start[0],6-start[1]),
+                new Move(start,  3-start[0],7-start[1]),
+                new Move(start,  2-start[0],8-start[1])
         );
+        return moves;
+    }
+    @Override
+    /**
+     * Find if the bishop move is legal in 5 or so operations
+     */
+    public boolean isValidMove(Move move){
+        return abs(move.getStart()[0] - move.getEnd()[0]) == abs(move.getEnd()[1] - move.getStart()[1]);
     }
     @Override
     public String toString(){
