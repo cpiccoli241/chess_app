578b5c9c1b031526554f922b42af9c41663a4eca
Chris Piccoli <cap9763@rit.edu>
2021-09-10 13:14:27 -0400

    Refactored CMDChessApp into a PGNConverter

--- src/main/java/CMDChessApp.java
+++ src/main/java/CMDChessApp.java
@@ -1,30 +1,19 @@
 import model.ChessBoardRep;
 import model.Color;
-import model.Move;
+import util.PGNConverter;
 
-import java.util.HashMap;
 import java.util.Scanner;
 
 public class CMDChessApp {
-    private static HashMap<Character, Integer> PGNConvert = new HashMap<>();
     private static boolean setup = false;
     public CMDChessApp(){
         if(!setup) {
-            addLetters();
+            PGNConverter.addLetters();
             setup = true;
         }
 
     }
-    private static void addLetters(){
-        PGNConvert.put('a',8);
-        PGNConvert.put('b',7);
-        PGNConvert.put('c',6);
-        PGNConvert.put('d',5);
-        PGNConvert.put('e',4);
-        PGNConvert.put('f',3);
-        PGNConvert.put('g',2);
-        PGNConvert.put('h',1);
-    }
+
     public static void takeTurn(Scanner scin, ChessBoardRep bd){
         if(bd.getTurn() == Color.WHITE)
             System.out.println("White Players Move!");
@@ -34,19 +23,11 @@ public class CMDChessApp {
         //@todo support truePGN movement for now require start square id Pa2a3
         //@todo find a good place for converting pgn to ints for now do it here
 
-        bd.makeMove(convertFromPGNToMove(input));
+        bd.makeMove(PGNConverter.convertFromPGNToMove(input));
     }
-    public static int[] convertFromPGNToSquare(String input){
-        return new int[]{Integer.parseInt(input.substring(1, 2)), PGNConvert.get(input.charAt(0))};
-    }
-    public static Move convertFromPGNToMove(String input){
 
-        int[] start = convertFromPGNToSquare(input.substring(1,3));
-        int[] end = convertFromPGNToSquare(input.substring(3,5));
-        return new Move(start,end[0],end[1]);
-    }
     public static void main(String[] args) {
-        addLetters();
+        PGNConverter.addLetters();
         ChessBoardRep gameBoard = new ChessBoardRep();
         Scanner sc = new Scanner(System.in);
         while (!gameBoard.endState()) {
--- src/main/java/util/PGNConverter.java
+++ src/main/java/util/PGNConverter.java
@@ -1,4 +1,34 @@
 package util;
 
+import model.Move;
+
+import java.util.HashMap;
+
 public class PGNConverter {
+    private static HashMap<Character, Integer> PGNConvert = new HashMap<>();
+    public void setup(){
+        addLetters();
+    }
+
+    public static void addLetters(){
+        PGNConvert.put('a',8);
+        PGNConvert.put('b',7);
+        PGNConvert.put('c',6);
+        PGNConvert.put('d',5);
+        PGNConvert.put('e',4);
+        PGNConvert.put('f',3);
+        PGNConvert.put('g',2);
+        PGNConvert.put('h',1);
+    }
+
+    public static int[] convertFromPGNToSquare(String input){
+        return new int[]{Integer.parseInt(input.substring(1, 2)), PGNConvert.get(input.charAt(0))};
+    }
+
+    public static Move convertFromPGNToMove(String input){
+
+        int[] start = convertFromPGNToSquare(input.substring(1,3));
+        int[] end = convertFromPGNToSquare(input.substring(3,5));
+        return new Move(start,end[0],end[1]);
+    }
 }
