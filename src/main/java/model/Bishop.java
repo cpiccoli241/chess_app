71c6a54c4fa66e25f19e96bac92e80d7f01712e5
Chris Piccoli <cap9763@rit.edu>
2021-09-21 13:52:09 -0400

    Fixed Bishops Get Candidate Moves being terrible, and Exploding (memory wise).

--- src/main/java/model/Bishop.java
+++ src/main/java/model/Bishop.java
@@ -26,37 +26,36 @@ public class Bishop extends Piece {
      */
     public List<Move> getCandidateMoves() {
         List<Move> moves = new ArrayList<>();
-        int[] end = Arrays.copyOf(getPosition(), 2);
-        // very grossly find all the bishop moves
-        while (end[0] <= ChessBoardRep.BOARD_LENGTH && end[1] <= ChessBoardRep.BOARD_LENGTH) {
-            end[0] = end[0] + 1;
-            end[1] = end[1] + 1;
-            moves.add(new Move(getPosition(), end));
+        int[] temp = Arrays.copyOf(getPosition(),2);
+        int[] diagonal1 = getStartingdiag(getPosition()[0], getPosition()[1]);
+        int[] diagonal2 = getStartingdiag(getPosition()[0], 8-getPosition()[1]);
+        while(diagonal1[0] <= 8 && diagonal1[1] <=8){
+            moves.add(Move.MoveEndStart(getPosition(), diagonal1, this));
+            diagonal1[0]++;
+            diagonal1[1]++;
         }
-
-        end = Arrays.copyOf(getPosition(), 2);
-        while (end[0] > 0 && end[1] > 0) {
-            end[0] = end[0] - 1;
-            end[1] = end[1] - 1;
-            moves.add(new Move(getPosition(), end));
-        }
-
-        end = Arrays.copyOf(getPosition(), 2);
-        while (end[0] <= ChessBoardRep.BOARD_LENGTH && end[1] > 0) {
-            end[0] = end[0] + 1;
-            end[1] = end[1] - 1;
-            moves.add(new Move(getPosition(), end));
-        }
-
-        end = Arrays.copyOf(getPosition(), 2);
-        while (end[0] > 0 && end[1] <= ChessBoardRep.BOARD_LENGTH) {
-            end[0] = end[0] + 1;
-            end[1] = end[1] - 1;
-            moves.add(new Move(getPosition(), end));
+        while(diagonal2[0] <= 8 && diagonal2[1] <=8){
+            moves.add(Move.MoveEndStart(getPosition(), diagonal2, this));
+            diagonal2[0]--;
+            diagonal2[1]++;
         }
         return moves;
     }
-
+    private int[] getStartingdiag(int pos1, int pos2){
+        // one of these will be negative or they will both be 0
+        int border1 = pos1 - pos2;
+        int border2 = pos2-pos1;
+        int[] diag = new int[2];
+        // finds the border tile
+        if(border2 >= border1) {
+            diag[0] = 1;
+            diag[1] = border2+1;
+        }else{
+            diag[1] = 1;
+            diag[0] = border1+1;
+        }
+        return diag;
+    }
     @Override
     /**
      * Find if the bishop move is legal in 5 or so operations
