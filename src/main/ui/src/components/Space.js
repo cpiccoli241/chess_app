3074a2bb82a5989254fb16a1d3c359458317194d
Chris Piccoli <cap9763@rit.edu>
2021-09-28 13:30:21 -0400

    Initial Commit of React Webapp + Dummy Space and Board Functional Components

---
+++ src/main/ui/src/components/Space.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Space() {
+    return (
+        <div className="Space">
+            <p>this is a Space</p>
+        </div>
+
+    );
+}
+
+export default Space;
